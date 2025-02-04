3288a7b80bcf9d7e2642eaed884d29284b0fb8b1
Dimitri Skliros <dp.skliros@gmail.com>
2025-02-04 13:34:36 +0000

    Improved src/utils/paths.py and scripts/get_sonia_rates.py

--- scripts/get_sonia_rates.py
+++ scripts/get_sonia_rates.py
@@ -1,11 +1,14 @@
-import os
-from src.utils.paths import PROJECT_ROOT, RAW_DATA_DIR
+from pathlib import Path
+import pandas as pd
+from src.utils.paths import RAW_DATA_DIR
 
+filepath = RAW_DATA_DIR / "sonia_raw.csv"
 
-if os.path.exists(f"{RAW_DATA_DIR}/SONIAa.csv"):
+if filepath.exists():
     print("SONIA rates were manually downloaded from the Bank of England website:")
     print("https://www.bankofengland.co.uk/boeapps/database/fromshowcolumns.asp")
-    print(f"and stored in `{RAW_DATA_DIR}` with filename SONIA.csv")
+    print(f"and stored in `{RAW_DATA_DIR}` with filename sonia_raw.csv")
+    data = pd.read_csv(filepath)
 else:
-    raise FileNotFoundError(f"The file SONIA.csv was not found in the directory: {RAW_DATA_DIR}. It needs to be obtained manually from the Bank of England website: https://www.bankofengland.co.uk/boeapps/database/fromshowcolumns.asp")
+    raise FileNotFoundError(f"The file sonia_raw.csv was not found in the directory: {RAW_DATA_DIR}. It needs to be obtained manually from the Bank of England website: https://www.bankofengland.co.uk/boeapps/database/fromshowcolumns.asp")
 
--- src/utils/paths.py
+++ src/utils/paths.py
@@ -2,21 +2,24 @@
 
 from pathlib import Path
 
-# Set project root directory
+
+# SET PROJECT ROOT
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
 
-# Set data directory relative to project root
+
+# DATA directories
 DATA_DIR = PROJECT_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
+INTERIM_DATA_DIR = DATA_DIR / "interim"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
 
-# Specify scripts, src and tests directories 
+
+# SCRIPTS, SRC, UTILS and TESTS directories 
 SCRIPTS_DIR = PROJECT_ROOT / "scripts"
 SRC_DIR = PROJECT_ROOT / "src"
+UTILS_DIR = SRC_DIR / "utils"
 TESTS_DIR = PROJECT_ROOT / "tests"
 
 
 if __name__ == "__main__":
-    print("Checking project root is correct:")
     print(f"PROJECT_ROOT = {PROJECT_ROOT}")
-
